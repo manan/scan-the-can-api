c6b8f60c0dd42e6e652b2d6106a723dff259ab56
Manan Mehta <mehtamanan@icloud.com>
2017-03-25 21:52:37 -0700

    bug fix

--- products/models.py
+++ products/models.py
@@ -7,6 +7,6 @@ class Product(models.Model):
     company = models.ForeignKey(Company, related_name="products")
     name = models.CharField(max_length=100)
     description = models.TextField(default="")
-    price = models.FloatField(default=0)
+    price = models.FloatField(default=0.0)
     barcode = models.CharField(max_length=True, unique=True)
     quantity = models.IntegerField(default=0)
