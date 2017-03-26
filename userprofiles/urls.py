ed954db9dbff91b2794c1f117cba97dbffdabc00
Manan Mehta <mehtamanan@icloud.com>
2017-03-25 20:26:59 -0700

    add token-auth url

--- userprofiles/urls.py
+++ userprofiles/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from rest_framework.authtoken import views as view
 
 urlpatterns = [
     # ADMIN PRIVILEGES
@@ -8,4 +9,5 @@ urlpatterns = [
     url(r'users/add/$', views.AddUser.as_view()),
     url(r'add/$', views.AddProfile.as_view()),
     url(r'self/$', views.SelfDetails.as_view()),
+    url(r'^token-auth/$', view.obtain_auth_token),
 ]
