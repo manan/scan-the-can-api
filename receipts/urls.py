dd84fba1b348169960c87d0d0131243cc1112366
Manan Mehta <mehtamanan@icloud.com>
2017-03-26 05:34:22 -0700

    added access point

--- receipts/urls.py
+++ receipts/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # ADMIN PRIVILEGES
     url(r'^$', views.ReceiptList.as_view()),
     url(r'^add/$', views.AddReceipt.as_view()),
-    url(r'^add/username=(?P<username>.+)/company=(?P<comp>.+)/$', views.add_products),
+    url(r'^add/username=(?P<username>.+)/company=(?P<comp>.+)/$', views.add_receipt),
     url(r'^addproducts/receipt=(?P<rec_id>.+)/barcodes=(?P<barcodes>.+)/$', views.add_products),
 ]
