1313199b6462c24848b641df6d30acdb4efc1872
Manan Mehta <mehtamanan@icloud.com>
2017-03-26 06:25:54 -0700

    debug

--- receipts/views.py
+++ receipts/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
@@ -49,7 +50,7 @@ def add_receipt(request, username, comp):
 @authentication_classes((TokenAuthentication,))
 @permission_classes((permissions.IsAuthenticated,))
 def add_products(request, rec_id, barcodes):
-    return JsonResponse('{"detail": "successful"}')
+    return HttpResponse('{"detail": "Operation successful."}', status=status.HTTP_200_OK)
     receipt = Receipt.objects.get(pk=rec_id)
     barcode_list = barcodes.replace(' ', '').split(',')
     for every_barcode in barcode_list:
